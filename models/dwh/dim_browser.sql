1fa2cdd34a12fac36bae78d858ee5bd2b2aedca0
Vijay <vijay.pal.86@gmail.com>
2024-06-11 11:10:45 +0200

    (new) Adding browser dimension

--- models/dwh/dim_browser.sql
+++ models/dwh/dim_browser.sql
@@ -1,21 +1,21 @@
 {{ config(
         materialized='incremental'
-        , unique_key = 'page_key'
+        , unique_key = 'browser_key'
         ) }}
 
-{% set cols_page_key = ['page_name'] %}
+{% set cols_browser_key = ['browser_name'] %}
 
 
 with source_data as (
-  select page_name, page_type, max(stg_inserted_dt) as stg_inserted_dt
+  select browser_used as browser_name, max(stg_inserted_dt) as stg_inserted_dt
   from {{ref('stg_fedex_case_data_web')}}
-  group by page_name, page_type
+  group by browser_used
+  order by browser_name
 )
 
-select distinct
-  {{ dbt_utils.generate_surrogate_key(cols_page_key) }} as page_key
-  , page_name
-  , page_type
+select
+  {{ dbt_utils.generate_surrogate_key(cols_browser_key) }} as browser_key
+  , browser_name
   , {{ dbt.current_timestamp() }}::timestamp with time zone as dwh_inserted_dt
 from source_data
 -- To avoid re-processing same staging data i.e., incase the stage data didnt change 
